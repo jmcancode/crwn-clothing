77c7613b95a270ababc97bcefad7d776ce7ab00d
Jon-Michael Narvaez <jm@stylexjm.com>
2020-05-18 14:49:13 -0500

    created smaller components

--- src/components/menu-item/menu-item.components.js
+++ src/components/menu-item/menu-item.components.js
@@ -3,12 +3,15 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 const MenuItem = ({title, imageUrl, size}) => (
-    <div style={{
+    <div 
+    className={`${size} menu-item`}>
+    <div 
+        className="background-image" 
+        style={{
         backgroundImage: `url(${imageUrl})`
-    }} 
-    className={`${size} menu-item`}
-    >
-     <div className='content'>
+    }}
+    />
+    <div className='content'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <span className='subtitle'>Shop</span>
     </div>
