9a8246f88b8aa758aac3a0e28ec9835550b4ded5
Jon-Michael Narvaez <jm@stylexjm.com>
2020-05-18 15:27:12 -0500

    less6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.components';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
+
 function App() {
   return (
-    <div> 
-    <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
