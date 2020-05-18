ca2eaf8ab27a9b2801556e8a8aba95f30d65ca4e
Jon-Michael Narvaez <jm@stylexjm.com>
2020-05-18 14:24:37 -0500

    updates 1

--- src/components/menu-item/menu-item.components.js
+++ src/components/menu-item/menu-item.components.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-const MenuItem = ({title}) => (
-    <div className='menu-item'>
-            <div className='content'>
-            <h1 className='title'>{title}</h1>
-            <span className='subtitle'>Shop</span>
-            </div>
-        </div>
+import './menu-item.styles.scss';
+
+const MenuItem = ({title, imageUrl, size}) => (
+    <div style={{
+        backgroundImage: `url(${imageUrl})`
+    }} 
+    className={`${size} menu-item`}
+    >
+     <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>Shop</span>
+    </div>
+ </div>
 );
 
 export default MenuItem;
--- src/pages/homepage/homepage.components.jsx
+++ src/pages/homepage/homepage.components.jsx
@@ -1,40 +1,14 @@
 import React from 'react';
 
+import Directory from '../../components/directory/directory.compenents';
+
 import './homepage.styles.scss';
 
 
 const HomePage = () => (
 <div className='hompage'>
-    <div className='directory-menu'>
-        
-            <div className='menu-item'>
-            <div className='content'>
-            <h1 className='title'>Jackets</h1>
-            <span className='subtitle'>Shop</span>
-            </div>
-        </div>
-        <div className='menu-item'>
-        <div className='content'>
-        <h1 className='title'>Sneakers</h1>
-        <span className='subtitle'>Shop</span>
-        </div>
-    </div>
-    <div className='menu-item'>
-    <div className='content'>
-    <h1 className='title'>Women</h1>
-    <span className='subtitle'>Shop</span>
-    </div>
-</div>
-<div className='menu-item'>
-<div className='content'>
-<h1 className='title'>Men</h1>
-<span className='subtitle'>Shop</span>
-</div>
-</div>
-    </div>
-</div>
-
-
+   <Directory/>
+   </div>
 );
 
 export default HomePage;
