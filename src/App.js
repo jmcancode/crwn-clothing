ae63709154245d297764e9e5813a51221abb5a34
Jon-Michael Narvaez <jm@stylexjm.com>
2020-05-18 10:06:11 -0500

    add files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+
+import HomePage from './hompage.component';
+
+function App() {
+  return (
+    <div> 
+    <HomePage />
+    </div>
+  );
+}
+
+export default App;
