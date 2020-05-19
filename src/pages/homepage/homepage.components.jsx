c547ec7cc6bc158e193ea8d4173aef0a04bd894a
Jon-Michael Narvaez <jm@stylexjm.com>
2020-05-19 08:45:59 -0500

    update our menu-items to navigate with Router HOC

--- src/pages/homepage/homepage.components.jsx
+++ src/pages/homepage/homepage.components.jsx
@@ -5,7 +5,7 @@ import Directory from '../../components/directory/directory.compenents';
 import './homepage.styles.scss';
 
 
-const HomePage = () => (
+const HomePage = ({ history}) => (
 <div className='hompage'>
    <Directory/>
    </div>
