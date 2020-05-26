2df590509bbffba2fbcfdac31bba0c728f944e5e
Jon-Michael Narvaez <jm@stylexjm.com>
2020-05-26 08:32:55 -0500

    cart.reducer update

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,14 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItem.id 
+        );
+
+        if (existingCartItem) {
+            return cartItems.map(cartItem =>
+                cartItem.id === cartItem.id 
+                ? {...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem
+                )
+        }
+
+        return [...cartItems, {...cartItemToAdd, quantity: 1 }]
+};
