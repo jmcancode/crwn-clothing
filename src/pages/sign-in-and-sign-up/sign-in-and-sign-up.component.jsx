2acf3975bca7ec18bc6265cee8f3d0ab6b2dcc21
Jon-Michael Narvaez <jm@stylexjm.com>
2020-05-28 16:12:55 -0500

    made better

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import SignIn from '../../components/sign-in/sign-in.components';
-import SignUp from '../../components/sign-up/sign-up.components';
-import './sign-in-and-sign-up.styles.scss';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
+
+import { SignInAndSignUpContainer } from './sign-in-and-sign-up.styles';
 
 const SignInAndSignUpPage = () => (
-    <div className='sign-in-and-sign-up'>
+  <SignInAndSignUpContainer>
     <SignIn />
-    <SignUp/>
-    </div>
-    );
+    <SignUp />
+  </SignInAndSignUpContainer>
+);
 
-    export default SignInAndSignUpPage;
-
+export default SignInAndSignUpPage;
