b5939f871fe58810a83a99821dfdbd64e7e0d742
Jon-Michael Narvaez <jon-michaelnarvaez@jms-macbook-pro.attlocal.net>
2020-08-05 20:58:59 -0500

    updates

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ if(process.env.NODE_ENV !== 'production') require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.use(bodyParser.json());
 app.user(bodyParser.urlencoded({ extended: true }));
